c119577ae219dd8c5a5e2853b523f6ce05369cd6
Sean Altemose <sean@seanaltemose.com>
2018-06-25 18:52:49 +0000

    automatically backed up by learn

--- db/migrate/20180625184910_create_movies.rb
+++ db/migrate/20180625184910_create_movies.rb
@@ -1,4 +1,9 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    add_column :movie, :title, :text
+    add_column :movie, :release_date, :datetime
+    add_column :movie, :director, :text
+    add_column :movie, :lead, :text
+    add_column :movie, :in_theaters, :text
   end
 end
