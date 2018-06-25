9f010b5f56367a42736be2d2accbe1b02e34639b
Sean Altemose <sean@seanaltemose.com>
2018-06-25 19:22:57 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ end
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     args.each do |arg, val|
-      Movie.send('#{arg}', val)
+      Movie.send('#{arg}'=, val)
     end
   end
 end
